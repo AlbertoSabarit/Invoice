5344e5ed72321086a778baacbf34e06d36494b9c
unknown <suroro14@gmail.com>
2023-11-09 22:16:58 +0100

    Item List RecyclerView

--- features/itemdetail/src/main/java/com/moronlu18/itemdetail/ui/ItemDetailFragment.kt
+++ features/itemdetail/src/main/java/com/moronlu18/itemdetail/ui/ItemDetailFragment.kt
@@ -24,4 +24,9 @@ class ItemDetailFragment : Fragment() {
         return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }

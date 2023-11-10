ae748db4ff8ca7edfbfad1036e6bf1510d050de9
Alberto Sabarit <al.alberto.sabarit.penalosa@iesportada.org>
2023-11-10 09:35:33 +0100

    toast

--- features/customerdetail/src/main/java/com/murray/customerdetail/ui/CustomerDetailFragment.kt
+++ features/customerdetail/src/main/java/com/murray/customerdetail/ui/CustomerDetailFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.murray.customerdetail.R
 import androidx.fragment.app.Fragment
 import com.murray.customerdetail.databinding.FragmentCustomerDetailBinding
 
@@ -21,6 +23,13 @@ class CustomerDetailFragment : Fragment() {
     ): View? {
         _binding = FragmentCustomerDetailBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnEditar.setOnClickListener() {
+            Toast.makeText(requireContext(), R.string.toastMessage, Toast.LENGTH_SHORT).show()
+        }
     }
 
 
